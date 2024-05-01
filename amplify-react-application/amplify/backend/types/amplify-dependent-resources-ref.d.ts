5a55e14a9270f09d3440efe84e4d553f23db69e3
Shriya Hireholi <hireholis@gmail.com>
2024-05-01 16:24:21 -0700

    initial commit for react app

---
+++ amplify-react-application/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "3divvycb8fb85a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ amplify-react-application/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import {Amplify} from 'aws-amplify';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsConfig from './aws-exports';
+
+Amplify.configure(awsConfig);
+
+function App() {
+  return (
+    <div className="App">
+      <Authenticator>
+        {({ signOut }) => (
+          <main>
+            <header className='App-header'>
+              <button 
+                onClick={signOut} 
+                style={{ 
+                  margin: '20px', 
+                  fontSize: '0.8rem', 
+                  padding: '5px 10px', 
+                  marginTop: '20px'
+                }}
+              >
+                Sign Out
+              </button>
+            </header>
+          </main>
+        )}
+      </Authenticator>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
