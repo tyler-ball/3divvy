b5017e8edfbfb664da2999b23c4edebf0e2a25e8
Shriya Hireholi <hireholis@gmail.com>
2024-05-04 13:19:16 -0700

    created home and market page layouts

--- amplify-react-application/src/App.js
+++ amplify-react-application/src/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
-import './App.css';
 import {Amplify} from 'aws-amplify';
-import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import {withAuthenticator} from '@aws-amplify/ui-react';
 import awsConfig from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Home from './pages/Home';
+import Market from './pages/Market';
+import NoPage from './pages/NoPage';
+import './styles/navStyle.css'
 
 Amplify.configure(awsConfig);
 
 function App() {
   return (
     <div className="App">
-      <Authenticator>
-        {({ signOut }) => (
-          <main>
-            <header className='App-header'>
-              <button 
-                onClick={signOut} 
-                style={{ 
-                  margin: '20px', 
-                  fontSize: '0.8rem', 
-                  padding: '5px 10px', 
-                  marginTop: '20px'
-                }}
-              >
-                Sign Out
-              </button>
-            </header>
-          </main>
-        )}
-      </Authenticator>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/market' element={<Market />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ amplify-react-application/src/pages/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import '../styles/homeStyle.css'
+import NavBar from '../components/NavBar';
+
+
+function generate(element) {
+    return [0, 1, 2].map((value) =>
+      React.cloneElement(element, {
+        key: value,
+      }),
+    );
+  }
+
+export default function Home () {
+    return (
+        <>
+            <NavBar />
+            <div className="request-container">
+                <h2>My Requests</h2>
+                <div className='list-container'>
+                    <List >
+                        {generate(
+                            <ListItem>
+                            <ListItemText
+                                primary="Single-line item"
+                            />
+                            </ListItem>
+                        )}
+                    </List>
+                </div>
+            </div>
+            <div className="jobs-container">
+                <h2>My Jobs</h2>
+                <div className='list-container'>
+                    <List>
+                        {generate(
+                            <ListItem>
+                            <ListItemText
+                                primary="Single-line item"
+                            />
+                            </ListItem>
+                        )}
+                    </List>
+                </div>
+            </div>
+        </>
+    )
+}
