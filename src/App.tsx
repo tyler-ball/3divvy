f9a2cf555c3411f06a79b91d0c7e8fd4340d46b1
Patrick McKeever <patrick.mckeever@protonmail.com>
2024-05-19 17:53:10 -0700

    Add router to amplify v2

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,14 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Market from './pages/Market.jsx';
+import NoPage from './pages/NoPage.jsx';
 
 const client = generateClient<Schema>();
 
 function App() {
-  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  // useEffect(() => {
-  //   client.models.Todo.observeQuery().subscribe({
-  //     next: (data) => setTodos([...data.items]),
-  //   });
-  // }, []);
-
-  // function createTodo() {
-  //   client.models.Todo.create({ content: window.prompt("Todo content") }, { authMode: "userPool" });
-  // }
-
-  // function deleteTodo(id: string) {
-  //   client.models.Todo.delete({ id })
-  // }
-
   const [jobs, setJobs] = useState<Array<Schema["Job"]["type"]>>([]);
 
   useEffect(() => {
@@ -31,46 +19,42 @@ function App() {
     });
   }, []);
 
-  // const requiredMaterialsTypes = client.enums.RequiredMaterials.values();
-
-  // TODO figure out enums
-
-  // function createJob(submitter: string, title: string, description: string, amountOffered: string, requiredMaterials: "PLA" | "NYLON") {
-  //   client.models.Job.create({
-  //     submitter: submitter,
-  //     title: title,
-  //     description: description,
-  //     amountOffered: parseFloat(amountOffered),
-  //     requiredMaterials: requiredMaterials,
-  //   }, { authMode: "userPool" });
-  // }
-
-  // function deleteJob(id: string) {
-  //   client.models.Job.delete({ id })
-  // }
-
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>{user?.signInDetails?.loginId}'s jobs</h1>
-          {/* <button onClick={createTodo}>+ new</button> */}
-          <ul>
-            {jobs.map((job) => (
-              <li key={job.id}>{job.title}</li>
-            ))}
-          </ul>
-          <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-              Review next step of this tutorial.
-            </a>
-          </div>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+	<BrowserRouter>
+    	<Authenticator>
+    	  {({ signOut, user }) => (
+    	    <main>
+			<nav>
+				<ul>
+					<li><Link to="/home">Home</Link></li>
+					<li><Link to="/market">MarketPlace</Link></li>
+				</ul>
+			</nav>
+			<Routes>
+			  <Route index element={<Home />} />
+			  <Route path='/home' element={<Home />} />
+			  <Route path='/market' element={<Market />} />
+			  <Route path="*" element={<NoPage />} />
+			</Routes>
+    	      <h1>{user?.signInDetails?.loginId}'s jobs</h1>
+    	      {/* <button onClick={createTodo}>+ new</button> */}
+    	      <ul>
+    	        {jobs.map((job) => (
+    	          <li key={job.id}>{job.title}</li>
+    	        ))}
+    	      </ul>
+    	      <div>
+    	        🥳 App successfully hosted. Try creating a new todo.
+    	        <br />
+    	        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+    	          Review next step of this tutorial.
+    	        </a>
+    	      </div>
+    	      <button onClick={signOut}>Sign out</button>
+    	    </main>
+    	  )}
+    	</Authenticator>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,5 @@
+export default function NoPage() {
+    return(
+        <h1>Not Found</h1>
+    )
+}
